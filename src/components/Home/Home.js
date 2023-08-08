a175e2929e5f1043e5f55468339f0a8316045776
Vivek Shah <Vivekchiragshah2004@gmail.com>
2023-08-08 12:40:32 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,28 @@
-import React from 'react'
-import styles from './Home.module.css'
+import React from "react";
+import styles from "./Home.module.css";
 
 const Home = () => {
-   
-    return (
-        <div className={styles.pageContainer}>
-            
-            <section className={styles.hero}>
-                <h1>Easiest Billing</h1>
-                <div className={styles.paragraph}>
-                   
-                    <p>Billing application made with MongoDB, Express, React & Nodejs</p>
-                </div>
-                <div className={styles.imgContainer}>
-                    <img src="https://res.cloudinary.com/almpo/image/upload/v1637241441/special/banner_izy4xm.png" alt="invoicing-app"/>
-                </div>
-            </section>
+  return (
+    <div className={styles.pageContainer}>
+      <section className={styles.hero}>
+        <h1>By VenV Developers</h1>
+        <div className={styles.paragraph}>
+          <p>
+            Billing application made for all with all the important features.
+            Click "Get Started" for the demo or contact{" "}
+            <a href="venv.in">VenV</a> for tailoring it to imbue it with your
+            unique touch.
+          </p>
         </div>
-    )
-}
+        <div className={styles.imgContainer}>
+          <img
+            src="https://res.cloudinary.com/almpo/image/upload/v1637241441/special/banner_izy4xm.png"
+            alt="Invoicing-app"
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
